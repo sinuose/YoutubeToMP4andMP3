f27279290ba50b486783d5942938bb713e7c6993
Martin J. <34733738+sinuose@users.noreply.github.com>
2024-07-22 16:41:26 -0700

    Add files via upload

--- mp3_editor.py
+++ mp3_editor.py
@@ -1,22 +1,21 @@
 import eyed3
 import os
 from tqdm import tqdm
+from pathlib import Path
 
 folder = input("Please Select Folder: ")
-file_list = os.listdir(folder)
+paths = sorted(Path(folder).iterdir(), key=os.path.getmtime)
 
 ARTIST = input('ARTIST: ')
 ALBUM = input('ALBUM:')
 
-i = 0
-for file in tqdm(file_list):
+for i in tqdm(range(len(paths))):
+    file = str(paths[i])
     # just a short list of parameters to the MP3 File. 
-    title = file.copy()
-    file = folder + '/' + file
+    title = file.split('/')[-1]
     audiofile = eyed3.load(file)
     audiofile.tag.artist = ARTIST
     audiofile.tag.album = ALBUM
     audiofile.tag.title = title
     audiofile.tag.track_num  = i
     audiofile.tag.save()
-    i += 1
